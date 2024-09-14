bdeb91bfb553fee6dfcb6e9d2eac006768b621df
irfan-007 <thanatos786687@gmail.com>
2024-09-14 19:31:53 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
-
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
+import WeatherDisplay from "./WeatherDisplay";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <WeatherDisplay data={{ temperature: 25, conditions: "Sunny" }} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/WeatherDisplay.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function WeatherDisplay(props) {
+  let data = props.data;
+  //   console.log(data.temperature);
+
+  return (
+    <div>
+      <p>
+        <span
+          style={data.temperature > 20 ? { color: "red" } : { color: "blue" }}
+        >
+          Tempratue:{data.temperature}
+        </span>
+      </p>
+      <p>Condition:{data.conditions}</p>
+    </div>
+  );
+}
